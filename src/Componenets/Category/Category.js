7a9d0972390cac29b7280091e9f461ea3f7cf55a
Mulugeta-Junior <mulugeta.kassa2112@gmail.com>
2025-01-31 22:03:39 +0300

    Initial commit

---
+++ src/Componenets/Category/Category.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Categorycard from './Categorycard'
+import {Categoryinfos} from "./Categoryinfos"
+import classes from './category.module.css'
+
+
+
+
+function Category() {
+  return (
+    <section className={classes.Category_container}>
+      {Categoryinfos.map((infos) => {
+        return <Categorycard data={infos} />;
+      })}
+    </section>
+  );
+}
+export default Category;
